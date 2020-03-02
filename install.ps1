dc4dbab8e3bc91b60248d9d6d7f3d676832c01a2
Edge F <dev.edfabre@gmail.com>
2020-03-02 10:44:21 -0500

    Auto Push

--- install.ps1
+++
@@ -1,6 +0,0 @@
-$modulespath = ($env:psmodulepath -split ";")[0]
-$manageprojectpath = "$modulespath\ManagePSProject"
-New-Item -Type Container -Force -path $manageprojectpath | out-null
-Copy-Item "ManagePSProject.psm1" $manageprojectpath
-New-ModuleManifest -Path "$manageprojectpath\ManagePSProject.psd1" -ModuleVersion "1.0" -Author "Edge Fabre" -Description "This project contains my ManagePSProject module which is used to maintain a powershell project during it's lifecycle."
-Publish-Module -Name "ManagePSProject" -NuGetApiKey "oy2gp3tcq45dkjfhji3qb2z2tehtwcke7b2gr6a6kqyjsa"
